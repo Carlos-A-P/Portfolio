499f507edfd3d571e6ac2b981f44f50e6620a61e
Carlos-A-P <pcarlos491@gmail.com>
2021-09-23 16:52:22 -0700

    added scroll for href links on page

---
+++ js/jq.js
@@ -0,0 +1,9 @@
+//=========adding space above id I want href to scroll to 
+
+$(document).on('click', 'a[href^="#"]', function (event) {
+    event.preventDefault();
+
+    $('html, body').animate({
+        scrollTop: $($.attr(this, 'href')).offset().top - 100
+    }, 500);
+});
