1283de745abcbd9049edb8b473135994237580bf
Carlos-A-P <pcarlos491@gmail.com>
2021-09-24 21:17:38 -0700

    Updated style.css, index, jq.js, and script.js
    - In style.css, I added a max width to the nav so that it doesn't look too spread out in larger screens and added a gap between nav links. I also added a transparent background when at the top of the screen and turns black once scrolled down which was done in jquery file where I added a function.
    - As for my index.html, I added some new headers to prep for the other sections I want to add: Demo and technologies used. I also changed the style for the buttons.
    - I made a ot of changes to my JavaScript because I wasn't getting the array that I wanted for one of my values. I figured out how to do it now but I need to to change all of the "made_width" values to its proper format and it'll work with the for loop that pushes the values I want into an array and I join them and push them into my html

--- js/jq.js
+++ js/jq.js
@@ -1,4 +1,4 @@
-//=========adding space above id I want href to scroll to 
+//=========adding space above id I want href to scroll to with smooth animation
 
 $(document).on('click', 'a[href^="#"]', function (event) {
     event.preventDefault();
@@ -6,4 +6,11 @@ $(document).on('click', 'a[href^="#"]', function (event) {
     $('html, body').animate({
         scrollTop: $($.attr(this, 'href')).offset().top - 100
     }, 500);
+});
+
+//=========transparent navbar
+
+// this basically says when user scrolls from the top 10px toggle class
+$(window).scroll(function () {
+    $('nav').toggleClass('scrolled', $(this).scrollTop() > 10);
 });
