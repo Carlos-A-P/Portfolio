1c1b1d17a70440322a78cc925aefcd7aeb0f0863
Carlos-A-P <pcarlos491@gmail.com>
2021-09-26 19:44:37 -0700

    updated: style.css, index.html, and script.js
    I added embeded links into my JSON object so that I don't have to make users load videos folder for the preview videos. I also added an email form and tried to add custom success and error signs but I wasn't able to at the moment.

--- js/script.js
+++ js/script.js
@@ -10,7 +10,8 @@ const projects = `[
             "1":"CSS", 
             "2":"JavaScript"
         },
-        "preview": "",
+        "preview": "<div style=\\"padding:82% 0 0 0;position:relative;\\"><iframe src=\\"https://player.vimeo.com/video/615312720?h=e2fb553814&amp;badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479\\" frameborder=\\"0\\" allow=\\"autoplay; fullscreen; picture-in-picture\\" allowfullscreen style=\\"position:absolute;top:0;left:0;width:100%;height:100%;\\" title=\\"coffee_roasters\\"></iframe></div><script src=\\"https://player.vimeo.com/api/player.js\\"></script>\",
+        
         "longer_description": "This was a tough challenge that tested a lot of my javascript skills and organizational skills. It was a lot more than I expected but overall a very educational experience. I learned that there are many ways to do things, but some may be much easier and efficient than others. For example, my accordion was comprised of many buttons, but I first started by using div elements along with click event listeners. I later discovered that it was very tedious to make it accessible and decided to go with buttons instead, which comprised its own challenges with inner tags. Overall, I wanted to make an app that was mainly functional and accessible, so I didn't focus too much on transitions. I enjoyed this project, and I certainly learned a lot from it."
     },
     {
@@ -25,7 +26,7 @@ const projects = `[
             "2":"JavaScript",
             "3":"API"
         },
-        "preview": "",
+        "preview": "<div style=\\"padding:82% 0 0 0;position:relative;\\"><iframe src=\\"https://player.vimeo.com/video/615315737?h=5dbc8956b8&amp;badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479\\" frameborder=\\"0\\" allow=\\"autoplay; fullscreen; picture-in-picture\\" allowfullscreen style=\\"position:absolute;top:0;left:0;width:100%;height:100%;\\" title=\\"github_api\\"></iframe></div><script src=\\"https://player.vimeo.com/api/player.js\\"></script>",
         "longer_description": "This is my first API project where I made an app able to gather information from github to display user profile information. I also learned how to use Figma design files in order to provide the most accurate measurements possible."
     },
     {
@@ -40,7 +41,7 @@ const projects = `[
             "2":"JavaScript",
             "3":"Local Storage"
         },
-        "preview": "",
+        "preview": "<div style=\\"padding:82% 0 0 0;position:relative;\\"><iframe src=\\"https://player.vimeo.com/video/615317334?h=11c4fda5ee&amp;badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479\\" frameborder=\\"0\\" allow=\\"autoplay; fullscreen; picture-in-picture\\" allowfullscreen style=\\"position:absolute;top:0;left:0;width:100%;height:100%;\\" title=\\"todo_app\\"></iframe></div><script src=\\"https://player.vimeo.com/api/player.js\\"></script>",
         "longer_description": "This challenge proved to be difficult. I learned a lot about for loops and foreach methods. I tried to connect this to a backend server such as Azure db but I found it rather difficult and resulted to using local storage which also got the results that I wanted. I thought it was a lot easier to use for loops for this but quickly learned that in order to use them I have to keep changing the original array. As for saving the user's list, I saved the inner html of the todo wrapper and store it in a local storage every time it gets updated but some of the event handlers. Something that I'd like to improve on it learning how to implement a functional drag and drop without having to use an open source code."
     },
     {
@@ -54,7 +55,7 @@ const projects = `[
             "1":"CSS", 
             "2":"JavaScript"
         },
-        "preview": "",
+        "preview": "<div style=\\"padding:82% 0 0 0;position:relative;\\"><iframe src=\\"https://player.vimeo.com/video/615318728?h=7a0bcc9e62&amp;badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479\\" frameborder=\\"0\\" allow=\\"autoplay; fullscreen; picture-in-picture\\" allowfullscreen style=\\"position:absolute;top:0;left:0;width:100%;height:100%;\\" title=\\"tip_calculator\\"></iframe></div><script src=\\"https://player.vimeo.com/api/player.js\\"></script>",
         "longer_description": "This was an extremely fun challenge where I got to practice more javascript functions and understand how to efficiently implement it in my projects. I also used the data attribute to link my grid buttons to my script.js file. I generally reserve the class attribute for my css and my id attributes for JavaScript, however, in this case I had multiple buttons so I used the data attribute. As far as what I learned in CSS, I learned how to used the focus pseudo-class to let the user see the tip percentage they are using."
     },
     {
@@ -68,7 +69,7 @@ const projects = `[
             "1":"CSS", 
             "2":"JavaScript"
         },
-        "preview": "",
+        "preview": "<div style=\\"padding:64.75% 0 0 0;position:relative;\\"><iframe src=\\"https://player.vimeo.com/video/615320341?h=8c3b7132ea&amp;badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479\\" frameborder\\"0\\" allow=\\"autoplay; fullscreen; picture-in-picture\\" allowfullscreen style=\\"position:absolute;top:0;left:0;width:100%;height:100%;\\" title=\\"room_homepage\\"></iframe></div><script src=\\"https://player.vimeo.com/api/player.js\\"></script>",
         "longer_description": "This challenge really tested my grid-layout skills and I tried to use the least amount of code possible. This is also the first project where I added animations and transitions which was really fun to play around with. Lastly, I was able to add smooth transitions between slides and header images smoothly in multiple screens."
     },
     {
@@ -82,7 +83,7 @@ const projects = `[
             "1":"CSS", 
             "2":"JavaScript"
         },
-        "preview": "",
+        "preview": "<div style=\\"padding:66.54% 0 0 0;position:relative;\\"><iframe src=\\"https://player.vimeo.com/video/615322739?h=47000b7843&amp;badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479\\" frameborder=\\"0\\" allow=\\"autoplay; fullscreen; picture-in-picture\\" allowfullscreen style=\\"position:absolute;top:0;left:0;width:100%;height:100%;\\" title=\\"countdown-timer\\"></iframe></div><script src=\\"https://player.vimeo.com/api/player.js\\"></script>",
         "longer_description": "In this challenge I used the a javascript code from Website CodePen Home Flip Clock & Countdown which is a clock made without a HTML so I mainly focused on prioritizing my CSS and changing styles using Devtools. I also added a few changes to the javascript to remove the second timer."
     },
     {
@@ -97,7 +98,7 @@ const projects = `[
             "2":"JavaScript",
             "3":"JSON"
         },
-        "preview": "",
+        "preview": "<div style=\\"padding:66.54% 0 0 0;position:relative;\\"><iframe src=\\"https://player.vimeo.com/video/615323739?h=6ac12a5a19&amp;badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479\\" frameborder=\\"0\\" allow=\\"autoplay; fullscreen; picture-in-picture\\" allowfullscreen style=\\"position:absolute;top:0;left:0;width:100%;height:100%;\\" title=\\"time-tracking-dashboard\\"></iframe></div><script src=\\"https://player.vimeo.com/api/player.js\\"></script>",
         "longer_description": "In this challenge I learned how to utilize a JSON file into my JavaScript. At first I thought that I would have to somehow link the data.json file to my javascript and somehow import the content. What I found to be most helpful was to change the file into a script.js file or simply copy and paste the object into quotation marks. I then set the object to a variable in order to use it in my function."
     },
     {
@@ -111,7 +112,7 @@ const projects = `[
             "1":"CSS", 
             "2":"JavaScript"
         },
-        "preview": "",
+        "preview": "<div style=\\"padding:66.54% 0 0 0;position:relative;\\"><iframe src=\\"https://player.vimeo.com/video/615325586?h=db7c15b0ba&amp;badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479\\" frameborder=\\"0\\" allow=\\"autoplay; fullscreen; picture-in-picture\\" allowfullscreen style=\\"position:absolute;top:0;left:0;width:100%;height:100%;\\" title=\\"testimonial-grid-section\\"></iframe></div><script src=\\"https://player.vimeo.com/api/player.js\\"></script>",
         "longer_description": "This challenge tested my position skills. It helps me improve on grid layout."
     },
     {
@@ -125,7 +126,7 @@ const projects = `[
             "1":"CSS", 
             "2":"JavaScript"
         },
-        "preview": "",
+        "preview": "<div style=\\"padding:66.54% 0 0 0;position:relative;\\"><iframe src=\\"https://player.vimeo.com/video/615326713?h=54facac2d5&amp;badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479\\" frameborder=\\"0\\" allow=\\"autoplay; fullscreen; picture-in-picture\\" allowfullscreen style=\\"position:absolute;top:0;left:0;width:100%;height:100%;\\" title=\\"interactive-pricing-component\\"></iframe></div><script src=\\"https://player.vimeo.com/api/player.js\\"></script>",
         "longer_description": "This challenged taught me some new ways to use the ::before and ::after pseudo classes and how to implement the equivalent of media queries in javascript."
     },
     {
@@ -139,7 +140,7 @@ const projects = `[
             "1":"CSS", 
             "2":"JavaScript"
         },
-        "preview": "",
+        "preview": "<div style=\\"padding:65.69% 0 0 0;position:relative;\\"><iframe src=\\"https://player.vimeo.com/video/615327651?h=d18c9ee9ab&amp;badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479\\" frameborder=\\"0\\" allow=\\"autoplay; fullscreen; picture-in-picture\\" allowfullscreen style=\\"position:absolute;top:0;left:0;width:100%;height:100%;\\" title=\\"fylo-storage-component\\"></iframe></div><script src=\\"https://player.vimeo.com/api/player.js\\"></script>",
         "longer_description": "This is project is a challenge to test my css skills. What I found most helpful was the clip-path style property and position property.  I used HTML and CSS to complete the project"
     },
     {
@@ -153,7 +154,7 @@ const projects = `[
             "1":"CSS", 
             "2":"JavaScript"
         },
-        "preview": "",
+        "preview": "<div style=\\"padding:65.81% 0 0 0;position:relative;\\"><iframe src=\\"https://player.vimeo.com/video/615333258?h=25ab115998&amp;badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479\\" frameborder=\\"0\\" allow=\\"autoplay; fullscreen; picture-in-picture\\" allowfullscreen style=\\"position:absolute;top:0;left:0;width:100%;height:100%;\\" title=\\"fylo-dark-theme-landing-page\\"></iframe></div><script src=\\"https://player.vimeo.com/api/player.js\\"></script>",
         "longer_description": "This challenge tested my position skills. It helped me improve my flex-box, grid and absolute positioning skills for my elements and background element. It was one of the biggest projects I've made so I had to make sure I maintained clean and organized code."
     },
     {
@@ -167,7 +168,7 @@ const projects = `[
             "1":"CSS", 
             "2":"JavaScript"
         },
-        "preview": "",
+        "preview": "<div style=\\"padding:66.54% 0 0 0;position:relative;\\"><iframe src=\\"https://player.vimeo.com/video/615336658?h=dc19472165&amp;badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479\\" frameborder=\\"0\\" allow=\\"autoplay; fullscreen; picture-in-picture\\" allowfullscreen style=\\"position:absolute;top:0;left:0;width:100%;height:100%;\\" title=\\"pricing-component-with-toggle\\"></iframe></div><script src=\\"https://player.vimeo.com/api/player.js\\"></script>",
         "longer_description": "This challenged helped me get familiar with event listeners and it helped test my css skills to make a slider."
     },
     {
@@ -181,7 +182,7 @@ const projects = `[
             "1":"CSS", 
             "2":"JavaScript"
         },
-        "preview": "",
+        "preview": "<div style=\\"padding:66.18% 0 0 0;position:relative;\\"><iframe src=\\"https://player.vimeo.com/video/615338062?h=4546032644&amp;badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479\\" frameborder=\\"0\\" allow=\\"autoplay; fullscreen; picture-in-picture\\" allowfullscreen style=\\"position:absolute;top:0;left:0;width:100%;height:100%;\\" title=\\"sunnyside-agency-landing-page\\"></iframe></div><script src=\\"https://player.vimeo.com/api/player.js\\"></script>",
         "longer_description": "This was a difficult challenge that really tested my CSS skills. I found it difficult to provide a flexible layout for all screen sizes so I limited the display to 375px mobile view and 1440px desktop view. However, I was more than happy to achieve a result that looked very similar to the end goal. In this challenge I learned about the filter property to change the color of my svg icons. I also learned how to implement a drop down navigation menu for mobile users."
     }
 ]`
@@ -259,6 +260,7 @@ let card_description = document.getElementById('card-description')
 let codeBtn = document.getElementById('viewCode')
 let liveBtn = document.getElementById('viewLive')
 let techList = document.getElementById('tech_List')
+let cardPreview = document.getElementById('card-preview')
 
 // function inserts the info into the modal: big description, code and live urls, and list of technologies
 function modalInfo(myCard) {
@@ -268,6 +270,8 @@ function modalInfo(myCard) {
         if (myCard === String(JSON.parse(projects)[i].title)) {
             // modal title = "title"
             modalTitle.innerText = String(JSON.parse(projects)[i].title)
+            // card preview video
+            cardPreview.innerHTML = JSON.parse(projects)[i].preview
             // card description = "longer_description"
             card_description.innerText = String(JSON.parse(projects)[i].longer_description)
             // href = "code"
@@ -292,4 +296,53 @@ var myInput = document.getElementById('myInput')
 myModal.addEventListener('shown.bs.modal', function () {
     myInput.focus()
 })
+
+
+// ============Email Form
+window.addEventListener("DOMContentLoaded", function () {
+    // get the form elements defined in your form HTML above
+  
+    var form = document.getElementById("my-form")
+    // var button = document.getElementById("my-form-button")
+    var status = document.getElementById("status")
+  
+    // Success and Error functions for after the form is submitted
+  
+    function success() {
+      form.reset()
+      status.classList.add("success")
+      status.innerHTML = "Thanks!"
+    }
+  
+    function error() {
+      status.classList.add("error")
+      status.innerHTML = "Oops! There was a problem."
+    }
+  
+    // handle the form submission event
+  
+    form.addEventListener("submit", function (ev) {
+      ev.preventDefault()
+      var data = new FormData(form)
+      ajax(form.method, form.action, data, success, error)
+    })
+  })
+  
+  // helper function for sending an AJAX request
+  
+  function ajax(method, url, data, success, error) {
+    var xhr = new XMLHttpRequest()
+    xhr.open(method, url)
+    xhr.setRequestHeader("Accept", "application/json")
+    xhr.onreadystatechange = function () {
+      if (xhr.readyState !== XMLHttpRequest.DONE) return
+      if (xhr.status === 200) {
+        success(xhr.response, xhr.responseType)
+      } else {
+        error(xhr.status, xhr.response, xhr.responseType)
+      }
+    }
+    xhr.send(data)
+  }
+  
 
